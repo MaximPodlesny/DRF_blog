242b0b80270536734d48ce24f19f4827c5cea341
MaximPodlesny <max070730@gmail.com>
2024-09-28 16:55:44 +0300

    first commit

---
+++ core/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from rest_framework import viewsets, permissions, pagination
+from .serializers import PostSerializer
+from .models import Post
+from rest_framework.response import Response
+
+
+class PageNumberSetPagination(pagination.PageNumberPagination):
+    page_size = 4
+    page_size_query_param = 'page_size'
+    ordering = 'created_at'
+
+class PostViewSet(viewsets.ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+    lookup_field = 'slug'
+    permission_classes = [permissions.AllowAny]
+    pagination_class = PageNumberSetPagination
+
+
